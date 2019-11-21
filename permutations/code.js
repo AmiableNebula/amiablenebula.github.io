b87e73aceae514fdcba0051ea4691a20ac9a42f6
Sarah <sarahramseymcneil@gmail.com>
2019-11-21 14:43:11 -0800

    Clean up JavaScript

--- permutations/code.js
+++ permutations/code.js
@@ -1,27 +1,13 @@
 // Returns the factorial of a given natural number
 function factorial (var n) {
-	if (n < 0) {
-		return -1;
-	} else if (n == 0 || n == 1) {
+	if (n == 1) {
 		return 1;
 	} else {
 		return n * factorial (n - 1);
 	}
 }
 
-// Returns the number of permunations of characters from a given string
+// Returns the number of permutations of characters from a given string
 function permutations (var str) {
-	if (str.length == 0) {
-		return 0;
-	} else if (str.length == 1) {
-		return 1;
-	} else {
-		// Keep track of the denominator
-		var denominator = 1;
-
-		// Iterate through the string
-		for (var i = 0; i < str.length; i++) {
-			// TODO: Figure out how the heck to implement the rest of this
-		}
-	}
+	// TODO: Figure out how to actually implement this moster
 }
