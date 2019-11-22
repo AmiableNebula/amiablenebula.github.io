150ca38403f882d062741cf2ff98b616d2b6375e
Sarah <sarahramseymcneil@gmail.com>
2019-11-22 14:56:29 -0800

    Finish JavaScript code

--- permutations/code.js
+++ permutations/code.js
@@ -1,13 +1,45 @@
 // Returns the factorial of a given natural number
 function factorial (var n) {
-	if (n == 1) {
+	if (n == 0 || n == 1) {
 		return 1;
 	} else {
 		return n * factorial (n - 1);
 	}
 }
 
-// Returns the number of permutations of characters from a given string
+// Returns the number of unique permutations of characters from a string
 function permutations (var str) {
-	// TODO: Figure out how to actually implement this moster
+	// Store the denominator and the length of the original string
+	var denominator = 1;
+	var length = str.length;
+
+	// Iterate through the string
+	for (var i = 0; i < str.length; i++) {
+		// Store and remove the current character
+		var current = str.chatAt (i);
+		str = str.substring (0, i) + str.substring (i + 1, str.length);
+		i--;
+
+		// Store how many times the current character occurs
+		var count = 1;
+
+		// Reiterate through the string
+		for (var j = 0; j < str.length; j++) {
+			// Check for duplicate characters
+			if (str.chatAt (j) == current) {
+				// Increment the counter
+				count++;
+
+				// Remove the repeated character from the string
+				str = str.substring (0, j) + str.substring (j + 1, str.length);
+				j--;
+			}
+		}
+
+		// Factor the character(s) into the denominator
+		denominator *= factorial (count);
+	}
+
+	// Calculate the number of unique permutations
+	return factorial (length) / denominator;
 }
